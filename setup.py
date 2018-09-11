6aaac357269cec8a7e6693777c9950d3086e1cd5
Abhishek chaudhary <shekyabhi2307@gmail.com>
2018-09-11 12:55:52 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My first datascience project',
+    author='abhishek2307',
+    license='MIT',
+)
